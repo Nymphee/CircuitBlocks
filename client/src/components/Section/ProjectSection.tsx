2087ba2159dc504c6fe6c6733311119301854287
Dorian Dugandžić <dorian-98@live.com>
2021-02-11 16:34:42 +0100

    Changed visuals for buttons delete/edit

--- client/src/components/Section/ProjectSection.tsx
+++ client/src/components/Section/ProjectSection.tsx
@@ -3,18 +3,22 @@ import React from 'react';
 import { Card, CardContainer } from '../Card';
 import Section from './Section';
 import {Devices, Sketch} from '../../layouts/Home';
+import {Button} from "semantic-ui-react";
+import path from "path";
 
 interface ProjectSectionProps {
   title: string;
   projects: { block: Sketch[], code: Sketch[] };
   onPress: (type: 'NEW' | 'OPEN', sketch?: Sketch) => void;
   createNew?: boolean;
+  deleteSketches: (name: string, path: string) => void;
 }
 
 const ProjectSection: React.FC<ProjectSectionProps> = (props: ProjectSectionProps) => {
   const { title, projects, onPress, createNew } = props;
 
-  return (
+
+    return (
     <Section>
       <h2>{title}</h2>
       <CardContainer>
@@ -28,8 +32,7 @@ const ProjectSection: React.FC<ProjectSectionProps> = (props: ProjectSectionProp
         )}
 
         {projects.block.map((sketch: Sketch, index) => (
-          <Card key={`section-${title}-${index}`} className={ "descriptive " + (Devices.hasOwnProperty(sketch.device) ? Devices[sketch.device].name.toLowerCase() : undefined) }
-            onClick={() => onPress('OPEN', sketch)}>
+          <Card key={`section-${title}-${index}`} className={ "descriptive " + (Devices.hasOwnProperty(sketch.device) ? Devices[sketch.device].name.toLowerCase() : undefined) }>
               <div className="image"><svg dangerouslySetInnerHTML={{ __html: sketch.snapshot || "" }} /></div>
             <div className="cover">
               <div className="title">{sketch.title}</div>
@@ -37,23 +40,29 @@ const ProjectSection: React.FC<ProjectSectionProps> = (props: ProjectSectionProp
                   <p className="device">{Devices[sketch.device].name}</p>
                   { sketch.description && <p>{sketch.description}</p> }
               </div>
+                <div>
+                    <Button primary onClick={() => onPress('OPEN', sketch)}>Edit</Button>
+                    <Button negative onClick={() => {
+                        props.deleteSketches(sketch.title, sketch.path as string);
+                    }}>Delete</Button>
+                </div>
             </div>
           </Card>
         ))}
 
-          {projects.code.map((sketch: Sketch, index) => (
-              <Card key={`section-${title}-${index}`} className={ "descriptive " + (Devices.hasOwnProperty(sketch.device) ? Devices[sketch.device].name.toLowerCase() : undefined) }
-                    onClick={() => onPress('OPEN', sketch)}>
-                  <div className="image code"><i className="material-icons"> code </i></div>
-                  <div className="cover">
-                      <div className="title">{sketch.title}</div>
-                      <div className="description">
-                          <p className="device">{Devices[sketch.device].name}</p>
-                          { sketch.description && <p>{sketch.description}</p> }
-                      </div>
+          {projects.code.map((sketch: Sketch, index) => <Card key={`section-${title}-${index}`} className={ "descriptive " + (Devices.hasOwnProperty(sketch.device) ? Devices[sketch.device].name.toLowerCase() : undefined) }>
+              <div className="image code"><i className="material-icons"> code </i></div>
+              <div className="cover">
+                  <div className="title">{sketch.title}</div>
+                  <div className="description">
+                      <p className="device">{Devices[sketch.device].name}</p>
+                      { sketch.description && <p>{sketch.description}</p> }
                   </div>
-              </Card>
-          ))}
+                  <Button primary onClick={() => onPress('OPEN', sketch)}>Edit</Button>
+                  <Button negative onClick={() => props.deleteSketches(sketch.title, sketch.path as string)}>Delete</Button>
+
+              </div>
+          </Card>)}
       </CardContainer>
     </Section>
   );
